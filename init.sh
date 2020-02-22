0436bd896d0ae1731f4682ee66832b7d0c09e81a
Daniel Horton Wellings <danielhortonwellings@gmail.com>
2020-02-21 21:51:57 -0600

    Fix init script

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 git clone git@github.com:danhort/docker-magento.git ops
 
-SOURCE_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")
-TARGET_DIRECTORY=$PWD
-
 # Copy the Makefile sample to the project root
 cp ops/samples/Makefile.sample ./Makefile
 
 # Copy the environment file to the ops directory
-cp ops/samples/docker.env.sample ./docker.env
+cp ops/samples/docker.env.sample ops/docker.env
+
+# Delete self 
+rm -- "$0"
